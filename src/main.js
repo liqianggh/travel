0dfb9c244d8da51477c516fac5abf970c316a2a7
Jann Lee <liqiang1chn@163.com>
2018-10-01 23:36:32 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+// 导入初始化css
+import './assets/styles/reset.css'
+// 1像素边框解决文件，兼容多倍屏幕
+import './assets/styles/border.css'
+// 导入fastClick
+import fastClick from 'fastclick'
 Vue.config.productionTip = false
-
+// fastclick 使用
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  router: router,
   components: { App: App },
   template: '<App/>'
 })
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="home">Hello Vue!</div>
+    <router-link  to="/list">列表页</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'home'
+}
+</script>
+<style>
+  .home{
+    font-size: 50px;
+  }
+</style>
