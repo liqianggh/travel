c5d34108ee22a2c593422785cf93f695ff81fe4a
Jann Lee <liqiang1chn@163.com>
2018-10-06 08:46:20 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,49 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">A</li>
+    <li class="item">B</li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'alphabet'
+}
+</script>
+<style lang="stylus" scoped>
+  @import "~styles/variables.styl"
+  .list
+    display: flex
+    flex-direction: column
+    justify-content center
+    position: absolute
+    top:1.58rem
+    right:0
+    width: .4rem
+    .item
+      text-align: center
+      line-height:.44rem
+      color: $bgColor
+
+</style>
