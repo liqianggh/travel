4eb3f9d16d79dadc3dece7d3740d2ad31d6eff45
Jann Lee <liqiang1chn@163.com>
2018-10-08 22:34:45 +0800

    header animation

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Head'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 <style lang="stylus" scoped>
+  .content
+    height: 50rem
 </style>
---
+++ src/pages/detail/components/Head.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <div  class="iconfont header-abs-back">&#xe624;</div>
+    </router-link>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <router-link to="/">
+        <div  class="iconfont header-fixed-back">&#xe624;</div>
+      </router-link>
+      景点详情
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      console.log(top + '___' + this.showAbs)
+      let opacity = top / 140
+      if (top > 60) {
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = {
+          opacity: opacity
+        }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import "~styles/variables.styl"
+  .header-abs
+    position:absolute
+    left:.2rem
+    top: .2rem
+    width: .8rem
+    height:.8rem
+    text-align: center
+    line-height:.8rem
+    border-radius:.4rem
+    background: #000
+    .header-abs-back
+      color: #fff
+      font-size:.4rem
+
+  .header-fixed
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    /*overflow: hidden*/
+    line-height:$headerHeight
+    height:$headerHeight
+    color: #fff
+    text-align: center
+    background: $bgColor
+    .header-fixed-back
+      position: absolute
+      top: 0
+      left: 0
+      width:.62rem
+      text-align: center
+      font-size:.4rem
+      color:#fff
+</style>
